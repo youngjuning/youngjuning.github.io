c68918d5003da81ad5f3d2a6d63b7a2450f1be8a
youngjuning <youngjuning@163.com>
2023-02-22 20:13:39 +0800

    优化

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -16,9 +16,11 @@ date: 2023-02-21 18:40:00
 comments: false
 description:
 type: galleryGroup
----\n
+---
+
+> [洛竹的图床](https://github.com/youngjuning/images)，用于存放一些图片，方便在其他地方使用。
 `;
-result += '{% gallery true %}\n';
+result += '\n{% gallery true %}\n';
 for (file of files) {
   result += file + '\n';
 }
