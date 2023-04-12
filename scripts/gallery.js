41901058bf3d6ecccb97be02367efcc1a4801d2e
youngjuning <luozhu2021@gmail.com>
2023-04-12 14:44:52 +0800

    更换标题

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -14,10 +14,14 @@ const files = fs
 
 let result = ''
 result += `---
-title: 图床
+title: 洛竹的图床
 date: 2023-02-21 18:40:00
 description: 洛竹的图床，用于存放一些图片，方便在其他地方使用。
 type: galleryGroup
+tags:
+  - 图床
+  - 洛竹的图床
+  - 图片
 ---
 
 > [洛竹的图床](https://github.com/youngjuning/images)，用于存放一些图片，方便在其他地方使用。
