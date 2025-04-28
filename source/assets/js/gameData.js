67ee1aa07ced4156e0cfac886ce1783b0a5ccc44
紫竹 <luozhu2021@gmail.com>
2025-04-28 14:09:55 +0800

    feat: add footer component and related styles/scripts

---
+++ source/assets/js/gameData.js
@@ -0,0 +1,335 @@
+var gameData = [
+    {
+        name: 'Beauty',
+        games: [
+            {
+                "gameId": 2524,
+                "gameName": "Baby Taylor Leg Injure",
+                "gameLogo": "https://gamefront.91onegame.com/134d54f8e1da41aba8272b6ce4b82ed6.webp"
+            },
+            {
+                "gameId": 1126,
+                "gameName": "Princess Happy Tea Party Cooking",
+                "gameLogo": "https://gamefront.91onegame.com/61b44f23ecf74b1185d7bf07dd104eab.Webp"
+            },
+            {
+                "gameId": 774,
+                "gameName": "Plumber Pipe",
+                "gameLogo": "https://gamefront.91onegame.com/9f64af8760e5406f899b3ddf99ffcb5a.Webp"
+            },
+            {
+                "gameId": 811,
+                "gameName": "King Bacon vs Vegans",
+                "gameLogo": "https://gamefront.91onegame.com/6d0a09ef34eb4e409b5fc0052d518175.Webp"
+            },
+            {
+                "gameId": 976,
+                "gameName": "Beach Kiss",
+                "gameLogo": "https://gamefront.91onegame.com/3ac92ada3433414c8f7af37aecca2838.Webp"
+            },
+            {
+                "gameId": 902,
+                "gameName": "Pocket Plants Garden",
+                "gameLogo": "https://gamefront.91onegame.com/cd837509282b4b03bff121fff63f448c.Webp"
+            },
+            {
+                "gameId": 901,
+                "gameName": "Magic Girl",
+                "gameLogo": "https://gamefront.91onegame.com/9f03c223c5884152a26c88dcd7e5a90e.webp"
+            },
+            {
+                "gameId": 853,
+                "gameName": "Extreme Followers",
+                "gameLogo": "https://gamefront.91onegame.com/e7fb7f6f9c174acf8f30688c473ecfc4.Webp"
+            }
+        ]
+    },
+    {
+        name: 'Puzzle',
+        games: [
+            {
+                "gameId": 2939,
+                "gameName": "Higgs Ball Paint",
+                "gameLogo": "https://gamefront.91onegame.com/518a3fcb04104799a08bb582da3fb2d6.png"
+            },
+            {
+                "gameId": 2935,
+                "gameName": "Road Connection",
+                "gameLogo": "https://gamefront.91onegame.com/b3b923fd30ec4851810814e0db5ebe72.png"
+            },
+            {
+                "gameId": 2933,
+                "gameName": "Save The Girl 3",
+                "gameLogo": "https://gamefront.91onegame.com/691a3294ca4c46e78f86076b0648a873.png"
+            },
+            {
+                "gameId": 2918,
+                "gameName": "Thief Puzzle Online",
+                "gameLogo": "https://gamefront.91onegame.com/0d2d7deb244d40e4823960f51fcde1b4.webp"
+            },
+            {
+                "gameId": 2911,
+                "gameName": "Drawing Lines Great Adventure",
+                "gameLogo": "https://gamefront.91onegame.com/537a67330dae46f4b0b7224a44a44ad9.webp"
+            },
+            {
+                "gameId": 2904,
+                "gameName": "Hoard Master Online",
+                "gameLogo": "https://gamefront.91onegame.com/9a940bc42f304968bb75c888d10cb0f3.webp"
+            },
+            {
+                "gameId": 2903,
+                "gameName": "Mahjong Story 2",
+                "gameLogo": "https://gamefront.91onegame.com/4853fe77d56b48e4a14b4bb006c8bf40.webp"
+            },
+            {
+                "gameId": 2900,
+                "gameName": "Protect The Dog 3D",
+                "gameLogo": "https://gamefront.91onegame.com/e085fbcd661a45e1b7fee2f10c8fba5c.webp"
+            }
+        ]
+    },
+    {
+        name: 'Sports',
+        games: [
+            {
+                "gameId": 2848,
+                "gameName": "Super Kick 3D World Cup",
+                "gameLogo": "https://gamefront.91onegame.com/4b72ae0d34464d20a9c635a1b39ad083.webp"
+            },
+            {
+                "gameId": 2693,
+                "gameName": "Climber Jump",
+                "gameLogo": "https://gamefront.91onegame.com/057db9cfb5d84c55a022dd033afad6bf.webp"
+            },
+            {
+                "gameId": 2690,
+                "gameName": "Tap Goal",
+                "gameLogo": "https://gamefront.91onegame.com/b87d57cf06b04845af9719c09eb140fa.webp"
+            },
+            {
+                "gameId": 2657,
+                "gameName": "Bikini Surfing Rescue",
+                "gameLogo": "https://gamefront.91onegame.com/f504839ea390404088a15b1211d428b0.webp"
+            },
+            {
+                "gameId": 2611,
+                "gameName": "Basket Battle",
+                "gameLogo": "https://gamefront.91onegame.com/25181f238ad24044bfbee47ab5ae2de1.webp"
+            },
+            {
+                "gameId": 2526,
+                "gameName": "Basketball Challenge",
+                "gameLogo": "https://gamefront.91onegame.com/7da59cfa3bac4fa6b4c27a8b280b5cd5.webp"
+            },
+            {
+                "gameId": 2525,
+                "gameName": "Fierce Shot",
+                "gameLogo": "https://gamefront.91onegame.com/afe79de83a13475eac3742aa06918461.webp"
+            },
+            {
+                "gameId": 2521,
+                "gameName": "Bouncy Dunks",
+                "gameLogo": "https://gamefront.91onegame.com/00d484e58f1f432f8c51e99917ac4944.webp"
+            }
+        ]
+    },
+    {
+        name: 'Home',
+        games: [
+            {
+                "gameId": 1901,
+                "gameName": "Long Skirt 3D",
+                "gameLogo": "https://gamefront.91onegame.com/79eb7e018ce04b61a41bba68035a3191.webp"
+            },
+            {
+                "gameId": 1899,
+                "gameName": "Paper Fold",
+                "gameLogo": "https://gamefront.91onegame.com/18ec827ef6064efd82467e9aa17799f8.webp"
+            },
+            {
+                "gameId": 1898,
+                "gameName": "Roll The Block",
+                "gameLogo": "https://gamefront.91onegame.com/0dc4f4bacb1545f192abb48870e8cee7.webp"
+            },
+            {
+                "gameId": 1897,
+                "gameName": "Winding Road",
+                "gameLogo": "https://gamefront.91onegame.com/1c55b8eefd7044409aa5b10045879f0c.webp"
+            },
+            {
+                "gameId": 1881,
+                "gameName": "Boat Hitting Out",
+                "gameLogo": "https://gamefront.91onegame.com/ee5e4638e8bd45a4bf813481bc8adaa3.webp"
+            },
+            {
+                "gameId": 1894,
+                "gameName": "Monster Wheels Apocalypse",
+                "gameLogo": "https://gamefront.91onegame.com/a64f272cb9bf4476a9787bbbe18c79f9.webp"
+            },
+            {
+                "gameId": 1891,
+                "gameName": "Bubble Fight.io",
+                "gameLogo": "https://gamefront.91onegame.com/d501770bde7246c2ba725505bc740c14.webp"
+            },
+            {
+                "gameId": 1890,
+                "gameName": "Brawl WarFire Online",
+                "gameLogo": "https://gamefront.91onegame.com/e9b00cd78bf44f96a0e35530c54c02d6.webp"
+            }
+        ]
+    },
+    {
+        name: 'Action',
+        games: [
+            {
+                "gameId": 2944,
+                "gameName": "Kitty Cats",
+                "gameLogo": "https://gamefront.91onegame.com/fc61d7b064b34d6da3830e916ce2c47f.webp"
+            },
+            {
+                "gameId": 2943,
+                "gameName": "Worms Zone A Slithery Snake",
+                "gameLogo": "https://gamefront.91onegame.com/32b49cb4ff684bb59ecc3c0eea691420.webp"
+            },
+            {
+                "gameId": 2942,
+                "gameName": "Virus Attack Merge Plane",
+                "gameLogo": "https://gamefront.91onegame.com/58ac78bf524743aeae61d8f4634862ce.png"
+            },
+            {
+                "gameId": 2941,
+                "gameName": "Mosquito Bites",
+                "gameLogo": "https://gamefront.91onegame.com/22462f57ec3b4daa9de04a757f0c031d.png"
+            },
+            {
+                "gameId": 2940,
+                "gameName": "Zombie Shooter 3D",
+                "gameLogo": "https://gamefront.91onegame.com/66c51d9165b74cd2b8ff9fd9ee83a053.png"
+            },
+            {
+                "gameId": 2938,
+                "gameName": "Float Runners",
+                "gameLogo": "https://gamefront.91onegame.com/ea9a27fb561447fe89eec52b998c1c52.png"
+            },
+            {
+                "gameId": 2937,
+                "gameName": "Mermaid Rush",
+                "gameLogo": "https://gamefront.91onegame.com/c8b1bd7dc508478fa2f6e18ca63a1c0b.png"
+            },
+            {
+                "gameId": 2936,
+                "gameName": "Beauty Race 2",
+                "gameLogo": "https://gamefront.91onegame.com/1dee2502d5da4d5c8bd4605fe4294f35.png"
+            }
+        ]
+    },
+    {
+        name: 'Best',
+        games: [
+            {
+                "gameId": 2571,
+                "gameName": "Idle Bear Island Polar Tycoon",
+                "gameLogo": "https://gamefront.91onegame.com/7eb0e66d97204642b50593887ac1355c.webp"
+            },
+            {
+                "gameId": 1714,
+                "gameName": "Jelly Boom",
+                "gameLogo": "https://gamefront.91onegame.com/0c863e57ae8747978fc53720e7a57f39.webp"
+            },
+            {
+                "gameId": 1713,
+                "gameName": "Jumping Girl",
+                "gameLogo": "https://gamefront.91onegame.com/8b6389ed0a104cd98332aac05c0d75a8.webp"
+            },
+            {
+                "gameId": 1711,
+                "gameName": "Funny Agent",
+                "gameLogo": "https://gamefront.91onegame.com/1b5d5c31200649b8a901f38ad93f6c83.webp"
+            },
+            {
+                "gameId": 1707,
+                "gameName": "Drift Race 3D",
+                "gameLogo": "https://gamefront.91onegame.com/2e5ba0774a2444e0bcad51a577c84f5f.webp"
+            },
+            {
+                "gameId": 1705,
+                "gameName": "Road Crash",
+                "gameLogo": "https://gamefront.91onegame.com/01dabbfecc3e40aaa29307873ee8aeef.webp"
+            },
+            {
+                "gameId": 1686,
+                "gameName": "Ropeman 3D",
+                "gameLogo": "https://gamefront.91onegame.com/f5b08a3f0f2441da85b07ee6ae89f40d.webp"
+            },
+            {
+                "gameId": 1673,
+                "gameName": "Paint Pop 3D",
+                "gameLogo": "https://gamefront.91onegame.com/15449f3dc81f4af8a8903d951810b6bb.webp"
+            }
+        ]
+    },
+]
+
+var popular = [
+    {
+        "gameId": 1886,
+        "gameName": "Hit Master Rush",
+        "gameLogo": "https://gamefront.91onegame.com/9e3940ffdf30404f812616f97d3f0f4f.webp"
+    },
+    {
+        "gameId": 1883,
+        "gameName": "Race Masters Rush",
+        "gameLogo": "https://gamefront.91onegame.com/94c6d63aeba040f69cb8a7c8217280fb.webp"
+    },
+    {
+        "gameId": 1885,
+        "gameName": "Cycling Hero",
+        "gameLogo": "https://gamefront.91onegame.com/1b6d3c01fd6344719a4d21ac1a3d0797.webp"
+    },
+    {
+        "gameId": 1884,
+        "gameName": "Drawmaster2",
+        "gameLogo": "https://gamefront.91onegame.com/5ff05b028dde4e698543cf2781570291.webp"
+    },
+    {
+        "gameId": 1898,
+        "gameName": "Roll The Block",
+        "gameLogo": "https://gamefront.91onegame.com/0dc4f4bacb1545f192abb48870e8cee7.webp"
+    },
+    {
+        "gameId": 1899,
+        "gameName": "Paper Fold",
+        "gameLogo": "https://gamefront.91onegame.com/18ec827ef6064efd82467e9aa17799f8.webp"
+    },
+    {
+        "gameId": 1900,
+        "gameName": "Cool Score",
+        "gameLogo": "https://gamefront.91onegame.com/33208dc685cf40f69fe4f850ee17b294.webp"
+    },
+    {
+        "gameId": 1901,
+        "gameName": "Long Skirt 3D",
+        "gameLogo": "https://gamefront.91onegame.com/79eb7e018ce04b61a41bba68035a3191.webp"
+    },
+    {
+        "gameId": 1720,
+        "gameName": "Fall Race 3D",
+        "gameLogo": "https://gamefront.91onegame.com/fe7f729089ad4ab2b815345019804de4.webp"
+    },
+    {
+        "gameId": 1724,
+        "gameName": "Agent J",
+        "gameLogo": "https://gamefront.91onegame.com/7fd4006b3479481fb221a2beb280a6e7.webp"
+    },
+    {
+        "gameId": 903,
+        "gameName": "Momo Pop",
+        "gameLogo": "https://gamefront.91onegame.com/264fbcbe7ef841aaa55ea482849484a1.Webp"
+    },
+    {
+        "gameId": 927,
+        "gameName": "Bump",
+        "gameLogo": "https://gamefront.91onegame.com/a67fd029ead8416ab042e3108227510c.Webp"
+    }
+]
